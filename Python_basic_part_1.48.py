9cabf8b45299346f5e2ab2331e3369ea9cef4d97
gko <marcingilko@gmail.com>
2022-01-23 08:16:38 +0100

    add 46-49

---
+++ Python_basic_part_1.48.py
@@ -0,0 +1,9 @@
+# 48. Write a Python program to parse a string to Float or Integer.
+
+a = "234.333"
+f = float(a)
+
+#najpierw trzeba stringa zaminic na float a dopiero potem na integera
+i = int(f)
+print(f)
+print(i)
