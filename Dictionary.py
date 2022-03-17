e6ef8993ae939527138b010eed71cdbdbaad90c1
gko <marcingilko@gmail.com>
2022-03-17 11:39:15 +0100

    update Dictionary exercises

--- Dictionary.py
+++ Dictionary.py
@@ -17,3 +17,85 @@
 # print(sorted_d)
 # sorted_d = dict(sorted(d.items(), key= operator.itemgetter(1), reverse=True))
 # print(sorted_d)
+
+# ------------------------------------------------------------------------
+# !! 2. Write a Python script to add a key to a dictionary.
+# Sample Dictionary : {0: 10, 1: 20}
+# Expected Result : {0: 10, 1: 20, 2: 30}
+
+# 1st my solution
+# dic = {0: 10, 1: 20}
+# dic[2]= 30
+# print(dic)
+
+# 2nd solution
+
+# d = {0: 10, 1: 20}
+# print(d)
+# d.update({2: 30})
+# print(d)
+
+# ------------------------------------------------------------------------
+# !!! 3. Write a Python script to concatenate following dictionaries to create
+# a new one.
+# Sample Dictionary :
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# dic4 = {}
+#
+# for d in (dic1, dic2, dic3):
+#     dic4.update(d)
+# print(dic4)
+
+# ------------------------------------------------------------------------
+# 4. Write a Python script to check whether a given key already exists in
+# a dictionary.
+
+# 1st my solution
+# d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# print(1 in d)
+# print(7 in d)
+
+# 2nd solution
+# d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# def is_key_present(x):
+#     if x in d:
+#         print(True)
+#     else:
+#         print(False)
+# is_key_present(5)
+# is_key_present(9)
+
+# ------------------------------------------------------------------------
+# 5. Write a Python program to iterate over dictionaries using for loops.
+
+# d = {'x': 10, 'y': 20, 'z': 30}
+#
+# for key, value in d.items():
+#     print(f"{key} : {value}")
+
+# ------------------------------------------------------------------------
+# !!! 6. Write a Python script to generate and print a dictionary that contains
+# a number (between 1 and n) in the form (x, x*x). Go to the editor
+# Sample Dictionary ( n = 5) :
+# Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+# 1st
+# n = 5
+# dictionary = {}
+# for x in range(1, n + 1):
+#     dictionary[x] = x*x
+# print(dictionary)
+
+# 2nd
+
+# dictionary = {}
+# for x in range(1, 6):
+#     dictionary.update({x : x*x})
+# print(dictionary)
